7e3482ad2cfa8ecfc8255a3582b5441a18d89cae
Vladislav <vla97657234@gmail.com>
2022-12-12 10:39:31 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/native_add_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.native_add_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/c_src/src/main.c
@@ -0,0 +1,14 @@
+#include "../main.h"
+
+int main(int argc, char const *argv[]) {
+    hello_world();
+    return 0;
+}
+
+void hello_world() {
+    printf("Hello World\n");
+}
+
+int hello_int() {
+    return 77;
+}
